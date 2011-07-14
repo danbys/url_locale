2d0dfc57640ee86c51da03dc2efc6e4af193a2ad
Dan Byström <dan.bystrom@gmail.com>
2011-07-14 23:00:25 +0200

    removed engine file

--- lib/url_locale/engine.rb
+++
@@ -1,9 +0,0 @@
-require "rails"
-module UrlLocale
-  class Engine < Rails::Engine
-    initializer 'url_locale' do |app|
-      #app.middleware.insert_before ActionDispatch::Static, RequestUnescape
-      app.middleware.insert_before ActionDispatch::Static, Middleware
-    end
-  end
-end
