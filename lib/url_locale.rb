db16296ff60cbf3093e7cea555e7517d83207fe5
Dan Byström <dan.bystrom@gmail.com>
2011-07-14 22:49:53 +0200

    Ready for github

--- lib/url_locale.rb
+++ lib/url_locale.rb
@@ -1,4 +1,38 @@
 require "url_locale/middleware"
 module UrlLocale  
-  require 'uri_locale/engine' if defined? Rails
+  
+  class << self
+    # Returns an <tt>I18n.available_locales - I18n.default_locale</tt>
+    def translations
+      @@translations ||= (I18n.available_locales - [I18n.default_locale]).map &:to_s
+    end
+
+    # Returns a Regexp to extract the locale from +request.host+
+    def subdomain_locales
+      @@subdomain_locales ||= Regexp.new "(^|\.)(#{translations*'|'})\."
+    end
+
+    # Returns a Regexp to extract the locale from +request.path+
+    def path_locales
+      @@path_locales ||= Regexp.new "^\/(#{translations*'|'})(\/|$)"
+    end
+    
+    def host_mode
+      @@mode = :host
+    end
+    
+    def mode
+      @@mode ||= :path
+    end
+    
+    def detect request
+      if translations.present?
+        if mode == :path
+          path_locales.match(request.path).try :[], 1          
+        else
+          subdomain_locales.match(request.host).try :[], 2
+        end
+      end || I18n.default_locale.to_s
+    end
+  end
 end
--- lib/url_locale/middleware.rb
+++ lib/url_locale/middleware.rb
@@ -1,19 +1,5 @@
 module UrlLocale
   class Middleware
-    # Returns an <tt>I18n.available_locales - I18n.default_locale</tt>
-    def self.translations
-      @@translations ||= (I18n.available_locales - [I18n.default_locale]).map &:to_s
-    end
-
-    # Returns a Regexp to extract the locale from +request.host+
-    def self.subdomain_locales
-      @@subdomain_locales ||= Regexp.new "(^|\.)(#{translations*'|'})\."
-    end
-
-    # Returns a Regexp to extract the locale from +request.path+
-    def self.path_locales
-      @@subdomain_locales ||= Regexp.new "^\/(#{translations*'|'})\/"
-    end
 
     def initialize app
       @app = app
@@ -22,13 +8,9 @@ module UrlLocale
     def call env
       status, headers, body = @app.call env
       request = ::Rack::Request.new env
-      if request.path !~ /^\/(javascripts|stylesheets|images)/
-        locale = if UriLocale.translations.present?
-          UriLocale.subdomain_locales.match(request.host).try(:[], 2) ||
-          UriLocale.path_locales.match(request.path).try(:[], 1) ||
-          I18n.default_locale.to_s
-        end
-        env['rack.locale'] = headers['Content-Language'] = I18n.locale = locale 
+      mime_types = env["action_dispatch.request.formats"] || []
+      if mime_types.find(&:html?).present?
+        env['rack.locale'] = headers['Content-Language'] = UrlLocale.detect(request)
       end
       [status, headers, body]
     end
